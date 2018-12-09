4080c2a45236eeeadf672f4c12cefe371b76fbd6
Egis <lukas.egidijus@gmail.com>
2018-12-09 13:20:10 +0000

    bumped fucss

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 module.exports.fucss = module.exports.cls = module.exports.classer = 
   function(obj){
+    // console.log({ obj });
     return Object.entries(obj).reduce((str, [ key, show ]) => !!(show) ? str + ' ' + key : str , '');
   }
