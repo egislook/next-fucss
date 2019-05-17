a1ae35d5693e59e79a477039f4ab574d440e0934
Egis <lukas.egidijus@gmail.com>
2019-05-17 05:12:10 +0000

    fixed console.log

--- utils.js
+++ utils.js
@@ -6,10 +6,10 @@ module.exports.fucss = module.exports.cls = module.exports.classer =
   
 module.exports.cssReload = function(link){
   if(typeof window === 'object'){
-    console.log('[FUCSS] Reload');
     link = link || '/main.css';
     const elem = document.querySelector('[href*="' + link + '"]');
     if(!elem) return;
+    console.log('[FUCSS] Reload');
     elem.href = link + '?' + new Date().getTime();
   }
 }
