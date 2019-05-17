bebc42dc379bfca59ca4e760a7ff075ff86905ee
Egis <lukas.egidijus@gmail.com>
2019-05-17 10:03:58 +0000

    fustyle added

--- utils.js
+++ utils.js
@@ -1,7 +1,21 @@
+const fucss = require('fucss');
+
 module.exports.fucss = module.exports.cls = module.exports.classer = 
   function(obj){
     // console.log({ obj });
     return Object.entries(obj).reduce((str, [ key, show ]) => !!(show) ? str + ' ' + key : str , '');
+  }
+  
+module.exports.fustyle = 
+  function(obj){
+    // console.log({ obj });
+    const classes = Object.entries(obj).reduce((arr, [ key, show ]) => (
+      show && key.length ? arr.concat(key.split(' ')) : arr
+    ), []);
+    
+    let style = fucss.generateStyling({ classes, returnStyle: true, onlyRules: true, escape: true, glob: false });
+    
+    return style;
   }
   
 module.exports.cssReload = function(link){
