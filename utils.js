f054bd79306f65279c01292979ca416a1a268562
Egis <lukas.egidijus@gmail.com>
2019-05-17 05:11:26 +0000

    fixed none existing element cssReload

--- utils.js
+++ utils.js
@@ -6,9 +6,10 @@ module.exports.fucss = module.exports.cls = module.exports.classer =
   
 module.exports.cssReload = function(link){
   if(typeof window === 'object'){
-    console.log('reload Style');
+    console.log('[FUCSS] Reload');
     link = link || '/main.css';
     const elem = document.querySelector('[href*="' + link + '"]');
+    if(!elem) return;
     elem.href = link + '?' + new Date().getTime();
   }
 }
