778e31eebea37c4575b7899ba22a74b034770142
noneedsystem <noneedsystem@gmail.com>
2019-10-06 23:31:32 +0700

    fucss configuration now can be passed to webpack

--- fucss.plugin.js
+++ fucss.plugin.js
@@ -4,57 +4,63 @@ const fs    = require('fs');
 const styleFile = process.cwd() + '/static/style.css';
 
 module.exports = class FucssPlugin {
-  
-  constructor(options) {
+
+  constructor(config) {
     // this.options = options;
     this.prevClasses = [];
+    if(!config)
+      return
+
+    for(let key in config){
+      fucss[key] = Object.assign(fucss[key], config[key])
+    }
   }
-  
+
   apply(compiler) {
-    
+
     compiler.hooks.beforeCompile.tapPromise('FucssPlugin', async (compilation) => {
       !fs.existsSync(styleFile) && fs.writeFileSync(styleFile, '', { flag: 'w+' } );
       return;
     })
-    
+
     compiler.hooks.emit.tapPromise('FucssPlugin', async (compilation, cb) => {
-      
+
       try{
-        
+
         const files = [];
-        
+
         if(!fucss.store.classes) return;
-        
+
         for(let i in compilation.chunks){
           const chunk = compilation.chunks[i];
           chunk.files.length && files.push(chunk.files[0]);
         }
-        
+
         const processingFile = files[files.length - 1];
-        
+
         if(~processingFile.indexOf('.css') || !fucss.store.classes.length)
           return;
-        
+
         const style = fucss.generateStyling({ classes: fucss.store.classes, returnStyle: true, glob: true, anim: true, escape: true });
-        
+
         // const str = `
-        //   prevLength: ${this.prevClasses.length} 
+        //   prevLength: ${this.prevClasses.length}
         //   currentLength: ${fucss.store.classes.length}
         //   processingFile: ${processingFile}
         //   classes: ${(fucss.store.classes || []).join(', ')}
         //   style: ${style}
         // `;
-          
+
         // fs.writeFileSync('./dump/plugin_' + new Date().getTime() + '.css', process.cwd() + '/static/style.css');
-        
+
         if(this.prevClasses.length === fucss.store.classes.length) return;
-        
+
         // let i = 100; while(i < 0){ console.log(process.cwd()); i--; };
-        
+
         fs.writeFileSync(styleFile, style + ' /** Incorrect classes ' + fucss.incorrect.join(' ') + ' */', { flag: 'w+' } );
         this.prevClasses = fucss.store.classes;
         return;
-        
+
       } catch(err){ console.log(err) }
     });
   }
@@ -62,12 +68,12 @@ module.exports = class FucssPlugin {
 
 function add(compilation, style, { filename, source }){
   const pageSource = [fucssInject.toString(), 'fucssInject("' + fucssToBase64(style) + '")', source].join('; ');
-    
+
   // fs.writeFileSync('temp.css', classes.join(', ') + pageSource, function(err, data){
   //   if (err) console.log(err);
   //   console.log("Successfully Written to File.");
   // });
-  
+
   compilation.assets[filename] = {
 		source: () => pageSource,
 		size: () => pageSource.length
@@ -78,24 +84,24 @@ function fucssInject(style){
   console.log(style);
   if(typeof document !== 'object')
     return;
-    
+
   var elem = document.querySelector('style#fucss');
-  
+
   style = window.atob(style.split('base64,').pop());
-  
+
   // console.log(style);
-  
-  if(elem){ 
+
+  if(elem){
     elem.innerHTML = style;
     return;
   }
-  
+
   // <link rel="stylesheet" href="css.css" media="none">
   // var css = document.createElement('link');
   // css.setAttribute('href', style);
   // css.setAttribute('rel', 'stylesheet');
   // css.setAttribute('id', 'fucss');
-  
+
   var css = document.createElement('style');
   css.setAttribute('id', 'fucss');
   css.type = 'text/css';
--- index.js
+++ index.js
@@ -6,27 +6,27 @@ const { cls }  = require('./utils.js');
 const PwaFucssPlugin = require('./pwa/pwa.fucss.plugin.js');
 const path = require('path');
 
-// module.exports = (mainConfig) => 
+// module.exports = (mainConfig) =>
 //   withCSS({
 //     webpack: (config, { dir, dev, isServer, defaultLoaders }) => {
-      
+
 //       config.module.rules.push();
-      
+
 //       config.plugins.push(new FucssPlugin({ options: true }));
-    
+
 //       return config;
 //     }
 //   })
 
-module.exports.withFucss = config => {
+module.exports.withFucss = (config, fucssConfig) => {
   config.module.rules.push(loader);
-  config.plugins.push(new FucssPlugin({ options: true }));
+  config.plugins.push(new FucssPlugin(fucssConfig));
   return config;
 }
 
 module.exports.plugin = FucssPlugin;
 module.exports.PwaFucssPlugin = PwaFucssPlugin;
-  
+
 const loader = module.exports.loader = {
   test: /\.(js|jsx)$/,
   exclude: [/node_modules/],
@@ -37,6 +37,6 @@ const loader = module.exports.loader = {
 module.exports.fucss = module.exports.cls = module.exports.classer = cls;
 
 // if(typeof window !== 'object'){
-  
-  
+
+
 // }
